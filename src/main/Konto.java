aa38c345dd0bf9d59746d979079d2f6542bb7d4a
MarcusCoding <marcus_voigt@gmx.net>
2020-11-26 14:09:06 +0100

    Initial commit

---
+++ src/main/Konto.java
@@ -0,0 +1,96 @@
+package main;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.BitSet;
+
+public class Konto {
+
+    private double kontostand;
+    private String iban;
+
+    public Konto(String iban) {
+        if(isIBANValid(iban)){
+            this.iban = iban;
+        }else{
+            this.iban = null;
+        }
+    }
+
+    public double getKontostand() {
+        return kontostand;
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void zahleEin(double betrag) {
+        //falls Betrag negativ, Einzahlung verhindern
+        if(betrag > 0){
+            kontostand += betrag;
+        }
+    }
+
+    public boolean ueberweisen(double betrag, Konto konto){
+
+        //Mein Kontostand zu niedrig
+        if(kontostand < betrag){
+            return false;
+        }
+
+        if(konto == null){
+            return false;
+        }
+
+        if(hebeAb(betrag)){
+            konto.zahleEin(betrag);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public boolean hebeAb(double betrag) {
+        if(kontostand < betrag){
+            return false;
+        }else{
+            if(betrag > 0){
+                kontostand -= betrag;
+                return true;
+            }else{
+                //Negative Beiträge können nicht abgehoben werden
+                return false;
+            }
+        }
+    }
+
+    public boolean isIBANValid(String IBAN){
+
+        if(IBAN == null){
+            return false;
+        }
+
+        if(IBAN.length() < 22){
+            return false;
+        }
+
+        //Ländercode replace
+        String countryValue = IBAN.substring(0, 2);
+        IBAN = IBAN.replace(countryValue, String.format("%d%d", Character.getNumericValue(countryValue.charAt(0)), Character.getNumericValue(countryValue.charAt(1))));
+        String first6Chars = IBAN.substring(0, 6);
+
+        //erste 6 Zeichen ersetzen
+        IBAN = IBAN.replace(first6Chars, "");
+
+        //finale Zahl zusammenbauen aus IBAN und den ersten 6 Stellen am Ende
+        IBAN = String.format("%s%s", IBAN, first6Chars);
+
+        //Konvertierung zu BigInt und Modulo 97
+        BigInteger parsedIBAN = new BigInteger(IBAN);
+        BigInteger modulo = parsedIBAN.mod(new BigInteger("97"));
+        return modulo.equals(new BigInteger("1"));
+    }
+}
+
---
+++ src/test/KontoTest.java
@@ -0,0 +1,146 @@
+package test;
+
+import main.Konto;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class KontoTest {
+
+    Konto konto1 = null;
+    Konto konto2 = null;
+    Konto konto3 = null;
+
+    static ArrayList<String> ibans = new ArrayList<String>() {{
+        //falsch
+        add("DE221001005001234456789");
+        add("AL902081100800000010393");
+        //richtig
+        add("DE12500105170648489890");
+        add("DE07123412341234123412");
+        add("DE68210501700012345678");
+        add("GB33210520201555555555");
+    }};
+
+    @BeforeEach
+    void initializeService() {
+        this.konto1 = new Konto("DE08700901001234567890");
+        this.konto2 = new Konto("DE41500105170123456789");
+        this.konto3 = new Konto("41500105170123456789");
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testIBANUebernommen() {
+        assertEquals("DE08700901001234567890", konto1.getIban());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testIBANNichtUebernommen() {
+        assertEquals(null, konto3.getIban());
+    }
+
+    // Kontostand am Anfang 0
+    @org.junit.jupiter.api.Test
+    public void testKontostandAmAnfang() {
+        assertEquals(0, konto1.getKontostand());
+    }
+
+    // einzahlen
+    @org.junit.jupiter.api.Test
+    public void testZahleEin() {
+        konto1.zahleEin(200);
+        assertEquals(200.00, konto1.getKontostand());
+    }
+
+    // auszahlen
+    @org.junit.jupiter.api.Test
+    public void testHebeAb() {
+        konto1.zahleEin(200);
+        konto1.hebeAb(150);
+        assertEquals(50.00, konto1.getKontostand());
+    }
+
+    // auszahlen Rückgabe true
+    @org.junit.jupiter.api.Test
+    public void testHebeAbTrue() {
+        konto1.zahleEin(200);
+        assertTrue(konto1.hebeAb(150));
+    }
+
+    // auszahlen Rückgabe false
+    @org.junit.jupiter.api.Test
+    public void testHebeAbFalse() {
+        konto1.zahleEin(200);
+        assertFalse(konto1.hebeAb(250));
+    }
+
+    // auszahlen wenn Betrag auf Konto zu gering
+    @org.junit.jupiter.api.Test
+    public void testUeberziehe() {
+        konto1.zahleEin(200);
+        konto1.hebeAb(250);
+        assertEquals(200, konto1.getKontostand());
+    }
+
+    //test cases: negative values
+    @org.junit.jupiter.api.Test
+    public void testNegativeZahleEin() {
+        konto1.zahleEin(-200);
+        assertEquals(0, konto1.getKontostand());
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testNegativeHebeAb() {
+        assertFalse(konto1.hebeAb(-200));
+    }
+
+    //neue Testfälle für Aufgabe 2
+
+    //Überweisung auf beliebiges anderes Konto
+    @org.junit.jupiter.api.Test
+    public void testUeberweisen(){
+        konto1.zahleEin(200);
+        konto1.ueberweisen(200, konto2);
+        assertEquals(200, konto2.getKontostand());
+    }
+
+    //Überweisung auf beliebiges anderes Konto
+    @org.junit.jupiter.api.Test
+    public void testUeberweisenFehler(){
+        konto1.zahleEin(150);
+        assertFalse(konto1.ueberweisen(200, konto2));
+    }
+
+    //Prüfung der IBAN auf Gültigkeit
+    @org.junit.jupiter.api.Test
+    public void testIsIBANValid() {
+        assertTrue(konto1.isIBANValid("DE08700901001234567890"));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testIsNullValid() {
+        assertFalse(konto1.isIBANValid(null));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testIsWrongIBANValid() {
+        assertFalse(konto1.isIBANValid("3357333"));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testIBANList() {
+        int correctIBAN = 0;
+
+        for(int i = 0; i < ibans.size(); i++){
+            if(konto1.isIBANValid(ibans.get(i)))
+            {
+                correctIBAN++;
+            }
+        }
+
+        assertEquals(3, correctIBAN);
+    }
+
+}
